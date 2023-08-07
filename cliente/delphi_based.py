61adbebb5eb6e394930093dd6a00488b4f7f56a1
matheus teixeira <matheusemox@gmail.com>
2023-08-07 08:09:52 -0300

    Update delphi_based.py
    funções principais

--- cliente/delphi_based.py
+++ cliente/delphi_based.py
@@ -879,3 +879,398 @@ begin
   end;
 end;
 '''
+
+# import socket
+# import sys
+# import time
+# import zlib
+# import threading
+# import concurrent.futures
+# from PIL import ImageGrab
+
+
+# # SERVER_HOST = "api.sdremoto.com.br"  # IP do servidor
+# SERVER_HOST ="10.200.0.15"  # Defina o IP do servidor aqui
+# SERVER_PORT = 6651       # Defina a porta do servidor aqui
+# PROCESSING_SLACK = 0.1
+
+# class DataCompressor:
+#     @staticmethod
+#     def compress(data):
+#         return zlib.compress(data)
+
+#     @staticmethod
+#     def decompress(data):
+#         return zlib.decompress(data)
+
+# class MessageHandler:
+#     def __init__(self, sock):
+#         self.sock = sock
+
+#     def send(self, message):
+#         self.sock.sendall(message.encode())
+
+#     def receive(self):
+#         data = self.sock.recv().decode()
+#         return data
+
+# class ChatHandler:
+#     def __init__(self, msg_handler):
+#         self.msg_handler = msg_handler
+
+#     def handle_chat(self):
+#         while True:
+#             try:
+#                 while data := self.msg_handler.receive():
+#                     pass
+#                 # Process received chat data
+#                 # ...
+
+#                 # Send chat data to GUI for display
+#                 # ...
+#             except Exception as e:
+#                 print(f"Error handling chat: {e}")
+#                 break
+
+# class ClipboardHandler:
+#     def __init__(self, msg_handler):
+#         self.msg_handler = msg_handler
+
+#     def handle_clipboard(self):
+
+#             try:
+#                 while data := self.msg_handler.receive():
+#                     pass
+#                 # Process received data from clipboard
+#                 # ...
+
+#                 # Send clipboard data to server
+#                 # ...
+#             except Exception as e:
+#                 print(f"Error handling clipboard: {e}")
+
+#     def close(self):
+#         pass
+
+# class KeyboardHandler:
+#     def __init__(self, msg_handler):
+#         self.msg_handler = msg_handler
+
+#     def handle_keyboard(self):
+#         try:
+#             while data := self.msg_handler.receive():
+#                 print("Controle do teclado iniciado.", data)
+#                 # Process received keyboard commands
+#                 # ...
+
+#                 # Simulate key presses on the client-side
+#                 # ...
+
+#         except Exception as e:
+#             print(f"Error handling keyboard: {e}")
+            
+#     def close(self):
+#         pass
+
+# class FileShareHandler:
+#     def __init__(self, msg_handler):
+#         self.msg_handler = msg_handler
+
+#     def handle_share_files(self):
+#         while True:
+#             try:
+#                 while data := self.msg_handler.receive():
+#                     pass
+#                 # Process received file list request or file data
+#                 # ...
+
+#                 # Send file list or file data to client
+#                 # ...
+#             except Exception as e:
+#                 print(f"Error handling file share: {e}")
+#                 break
+    
+# class DesktopHandler:
+#     def __init__(self, msg_handler):
+#         self.msg_handler = msg_handler
+
+#     def handle_desktop(self):
+#         # Helper functions for capturing the screen and sending image data
+#         def get_screen_as_bytes():
+#             screen = ImageGrab.grab()
+#             screen_bytes = screen.tobytes()
+#             return screen_bytes
+
+#         def compress_data(data):
+#             return DataCompressor.compress(data)
+
+#         # while True:
+#         #     try:
+#         #         data = self.receiver.receive()
+#         #         if not data:
+#         #             break
+
+#         #         # Process received desktop screen commands
+#         #         # ...
+
+#         #         # Capture the screen and compress image data
+#         #         screen_data = get_screen_as_bytes()
+#         #         compressed_data = compress_data(screen_data)
+
+#         #         # Send compressed image data to the server
+#         #         self.sender.send(compressed_data)
+#         #     except Exception as e:
+#         #         print(f"Error handling desktop screen: {e}")
+#         #         break
+
+# class ConnectionManager:
+#     def __init__(self):
+#         self.connections = []
+
+#     def add_connection(self, connection):
+#         self.connections.append(connection)
+
+#     def close_connections(self):
+#         for connection in self.connections:
+#             connection.close()
+             
+# class ConnectionThreads:
+#     def __init__(self, server_host, server_port):
+#         self.server_host = server_host
+#         self.server_port = server_port
+#         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#         self.msg_handler = MessageHandler(self.client_socket)
+
+#         self.connection_manager = ConnectionManager()
+#         self.chat_handler = ChatHandler(self.msg_handler)
+#         self.clipboard_handler = ClipboardHandler(self.msg_handler)
+#         self.keyboard_handler = KeyboardHandler(self.msg_handler)
+#         self.file_share_handler = FileShareHandler(self.msg_handler)
+#         self.desktop_handler = DesktopHandler(self.msg_handler)
+
+#     def main(self):
+        
+#         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+#             executor.submit(self.chat_handler.handle_chat)
+#             executor.submit(self.clipboard_handler.handle_clipboard)
+#             executor.submit(self.keyboard_handler.handle_keyboard)
+#             executor.submit(self.file_share_handler.handle_share_files)
+#             executor.submit(self.desktop_handler.handle_desktop)
+
+#             self.setup_connection()
+
+#     def setup_connection(self):
+#         self.client_socket.connect((self.server_host, self.server_port))
+#         self.connection_manager.add_connection(self.msg_handler)
+#         self.connection_manager.add_connection(self.chat_handler)
+#         self.connection_manager.add_connection(self.clipboard_handler)
+#         self.connection_manager.add_connection(self.keyboard_handler)
+#         self.connection_manager.add_connection(self.file_share_handler)
+#         self.connection_manager.add_connection(self.desktop_handler)
+
+#     def handle_initial_connection(self, target_id):
+#         if not target_id == '   -   -   ':
+#             if target_id == MyID:
+#                 print('You can not connect with yourself!')
+#             else:
+#                 self.msg_handler.send(target_id)
+#                 # Você pode adicionar o resto da lógica aqui
+#                 # ...
+#                 print('Finding the ID...')
+#                 self.reconnect()
+
+#     def reconnect(self):
+#         if not self.client_socket:
+#             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#             self.client_socket.connect((self.server_host, self.server_port))
+#             self.msg_handler = MessageHandler(self.client_socket)
+
+#             # Adicione o novo MessageHandler ao connection manager
+#             self.connection_manager.add_connection(self.msg_handler)
+
+#     def reconnect_secondary_sockets(self):
+#         self.desktop_handler.close()
+#         self.keyboard_handler.close()
+#         self.file_share_handler.close()
+
+#         # Aguarde a desconexão
+#         time.sleep(1)
+
+#         # Recrie os sockets secundários
+#         self.desktop_handler = DesktopHandler(self.msg_handler)
+#         self.keyboard_handler = KeyboardHandler(self.msg_handler)
+#         self.file_share_handler = FileShareHandler(self.msg_handler)
+
+#         self.connection_manager.add_connection(self.desktop_handler)
+#         self.connection_manager.add_connection(self.keyboard_handler)
+#         self.connection_manager.add_connection(self.file_share_handler)
+
+#         # Comece a tratar os sockets secundários novamente
+#         concurrent.futures.Executor.submit(self.desktop_handler.handle_desktop)
+#         concurrent.futures.Executor(self.keyboard_handler.handle_keyboard)
+#         concurrent.futures.Executor(self.file_share_handler.handle_share_files)
+
+#     def close(self):
+#         self.connection_manager.close_connections()
+
+
+# if __name__ == "__main__":
+#     client = ConnectionThreads(SERVER_HOST, SERVER_PORT)
+#     client.main()
+#     client.close()    
+
+
+# # import socket
+# # import threading
+
+# # HOST ="10.200.0.15"  # Defina o IP do servidor aqui
+# # PORT = 6651  
+
+# # class Princial():
+# #     def __init__(self):
+# #         self.data=''
+# #         self.id=''
+# #         self.password=''
+        
+# #     def receber_mensagens(self,sock):
+# #         while True:
+# #             try:
+# #                 data = sock.recv(1024).decode("utf-8")
+# #                 if not data:
+# #                     print("Conexão fechada pelo servidor.")
+# #                     break
+                
+# #                 print("Recebido:", data)
+# #                 self.data=str(data)
+
+# #                 # Define a variável de controle para indicar que uma nova mensagem foi recebida
+# #                 self.nova_mensagem = True
+
+# #                 # Chama o callback para lidar com a mensagem recebida
+# #                 self.process_data(data,sock)
+                
+# #             except Exception as e:
+# #                 print("Ocorreu um erro ao receber dados:", e)
+# #                 break
+            
+# #     def process_data(self, data,cliente_socket):
+#         # print(data)
+#         # if data.startswith('<|ID|>'):
+#         #     datalist= data.split('<|>')
+#         #     # Remove os caracteres '<', '|' e '>'
+#         #     print(datalist)
+#         #     self.id = datalist[0].replace('<|ID|>', '').replace('-', '')
+#         #     self.password = datalist[1].replace('<|END|>', '').replace('-', '')
+
+#         #     print("ID:", self.id)
+#         #     print("Password:", self.password)
+
+#         #     self.enviar_sockets(cliente_socket, self.id)
+            
+# #         elif data.startswith('<|PING|>'):
+            
+# #             cliente_socket.send(f'<|PONG|>'.encode("utf-8"))
+            
+            
+# #         else:
+# #             print('não processou nada')
+# #             return None
+        
+#     # def enviar_sockets(self,cliente_socket, id_str):
+#     #     try:
+#     #         cliente_socket.send(f'<|DESKTOPSOCKET|>{id_str}<|END|>'.encode("utf-8"))
+
+#     #         cliente_socket.send(f'<|FILESSOCKET|>{id_str}<|END|>'.encode("utf-8"))
+  
+#     #         cliente_socket.send(f'<|KEYBOARDSOCKET|>{id_str}<|END|>'.encode("utf-8"))
+
+#     #     except Exception as e:
+#     #         print("Ocorreu um erro ao enviar sockets:", e)    
+    
+# #     def main(self):
+# #         # Cria um objeto de socket
+# #         cliente_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+# #         try:
+# #             # Conecta-se ao servidor
+# #             cliente_socket.connect((HOST, PORT))
+# #             print("Conexão estabelecida com o servidor.")
+
+# #             # Inicia uma thread para receber mensagens do servidor
+# #             thread_receber = threading.Thread(target=self.receber_mensagens, args=(cliente_socket,))
+# #             thread_receber.start()
+            
+# #             # Aguarda um tempo para permitir a recepção de dados do servidor antes de processá-los
+# #             thread_receber.join(timeout=2)
+            
+# #             if thread_receber.is_alive():
+# #                 cliente_socket.send(b'<|MAINSOCKET|>')
+# #                 # Processa os dados recebidos
+                
+# #         except Exception as e:
+# #             cliente_socket.close()
+# #             print("Ocorreu um erro:", e)
+
+# #         # finally:
+# #         #     cliente_socket.close()
+# #         #     print("Conexão encerrada.")
+
+# # if __name__ == "__main__":
+
+# #     princ=Princial()
+# #     princ.main()
+
+# # Função para interagir com o UAC (User Account Control)
+# def interact_with_uac():
+#     hDesktop = win32gui.OpenInputDesktop(0, True, win32con.MAXIMUM_ALLOWED)
+#     if hDesktop != 0:
+#         win32gui.SetThreadDesktop(hDesktop)
+#         win32gui.CloseHandle(hDesktop)
+
+    # def setup_connection(self):
+    #     self.client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+    #     # Envia uma mensagem inicial para o servidor para obter o ID e a senha
+    #     self.client_socket.send(b'<|MAINSOCKET|>')
+        
+    #     # # Recebe os dados do servidor
+    #     # data = self.client_socket.recv(1024).decode("utf-8")
+    #     # Se receber um ID, está online
+    #     buffer = self.client_socket.recv(1024).decode("utf-8")
+    #     id_ = ''
+    #     position = data.find('<|ID|>')
+        
+    #     if data.startswith('<|ID|>'):
+    #         datalist= data.split('<|>')
+    #         # Remove os caracteres '<', '|' e '>'
+    #         print(datalist)
+    #         id_ = datalist[0].replace('<|ID|>', '').replace('-', '')
+    #         password_ = datalist[1].replace('<|END|>', '').replace('-', '')
+
+    #         print("ID:", id_)
+    #         print("Password:", password_)
+        
+    #         try:
+    #             # Cria instâncias separadas de socket para cada tipo de conexão
+    #             desktop_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #             keyboard_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #             files_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #             # Conecta os sockets usando a mesma porta para todas as conexões
+    #             desktop_socket.connect((self.server_host, self.server_port))
+    #             keyboard_socket.connect((self.server_host, self.server_port))
+    #             files_socket.connect((self.server_host, self.server_port))
+
+                # # Envia o ID para cada conexão para identificação do cliente
+                # desktop_socket.send(f'<|DESKTOPSOCKET|>{id_}<|END|>'.encode("utf-8"))
+                # # listen_messages()
+                # keyboard_socket.send(f'<|FILESSOCKET|>{id_}<|END|>'.encode("utf-8")) 
+                # # listen_messages()
+                # files_socket.send(f'<|KEYBOARDSOCKET|>{id_}<|END|>'.encode("utf-8"))
+                # # listen_messages()
+                
+    #             print('conexão realizada!')
+    #         except Exception as e:
+    #             print("Ocorreu um erro ao enviar sockets:", e)
+            
+    #         return id_, password_ 
